1d006036f303126905f4cbd7820bd8a2cb05b537
梅宮 <euthanasia045@gmail.com>
2018-12-03 13:11:52 +0900

    get webgl

--- webdrive/sushi/driver.py
+++ webdrive/sushi/driver.py
@@ -2,8 +2,11 @@ import os
 import yaml
 from time import sleep
 from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
 
-class Browser:
+from alert import Alert
+
+class Browser(Alert):
 
     def __init__(self):
         with open('param.yml', 'r') as param:
@@ -11,13 +14,26 @@ class Browser:
         try:
             self.driver = webdriver.Chrome()
         except:
-            raise ValueError('Cant booting browser.')
+            Alert.danger_alert('Cant booting browser.')
+            raise ValueError()
         else:
-            print('driver startup.')
+            Alert.safe_alert('driver startup.')
+            self.items = {}
 
 
     def redirect_url(self, unique=None):
         if not unique:
             self.driver.get(self.param_dict['sushi']['url'])
+            self.driver.set_page_load_timeout(5)
         else:
             self.driver.get(unique)
+
+
+    #if you want to get new app(webGL ver), execute this func at 0 argument.
+    #else, u wanto get old one, plz pass argument {cname}.
+    def get_app(self, cname='#canvas'):
+        try:
+            self.items['sushida_webgl'] = self.driver.find_element_by_class_name(cname)
+        except:
+            Alert.danger_alert('Cant find app.')
+
